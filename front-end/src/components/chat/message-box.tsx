071f6f63c339fbee845e00c5c552a3232cae1c5d
DanaY326 <danayuan2006@gmail.com>
2025-07-22 04:15:04 -0400

    Message box w/o image

--- front-end/src/components/chat/message-box.tsx
+++ front-end/src/components/chat/message-box.tsx
@@ -12,14 +12,6 @@ export default function MessageBox(props: Props) {
   const { close } = props;
   return (
     <div className="fixed bottom-5 right-5 w-100 h-120 pt-50 rounded-2xl bg-black outline-1 outline-solid outline-neutral-700 font-bold p-1 z-30">
-        <div className="absolute top-3 left-3 p-1 z-50 bg-blue-200 outline-1 outline-solid outline-neutral-700 rounded-lg">
-            <Image
-                src={"gemini-blue.svg"}
-                alt="Image"
-                height={20}
-                width={20}
-            />
-        </div>
         <CloseButton close={close}/>
     </div>
   );
